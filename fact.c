0db6a0c5984dfbd0810a20417fae2e9d39434409
HuzaifaEngr <user427b@gmail.com>
2024-11-03 12:48:55 +0500

    Update fact.c
    update efficiency of this program!

--- fact.c
+++ fact.c
@@ -2,20 +2,19 @@
 #include<conio.h>
 void main()
 {
-    //this program display the factorisation
-    long n , q , d = 2, r[10], f, g;
+    //this program display the factorization
+    long n , d = 2;
     printf("Enter a number :");
-    scanf("%d", &n);
-    for(long i = 0;i <= n; i++)
+    scanf("%ld", &n);
+    while(n > 1)
     {
-        for(long j = 1; n % d != 0; j++)
+        while(n % d != 0)
         {
             d++;
         }
 
         printf("%d\t|\t%d \n", d, n);
-        q = n / d;
-        n = q;
+        n /= d;
     }
     getch();
 }
